c40c5087afc5b08d3d7fab18f2909a1478e52819
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-29 19:42:35 +0600

    code update

--- index.js
+++ index.js
@@ -194,6 +194,14 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
     });
+    app.get('/payments', verifyTokens, async (req, res) => {
+      const query = { email: req.query.email };
+      if (req.params.email === req.decoded.email) {
+        return res.status(403).send('forbidden access');
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post('/payments', async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
